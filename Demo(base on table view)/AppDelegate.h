d4a3d68ecd5ddc0eb4bfe7637c22fb9f99295001
frank.xu <frank.xu@touch-spring.com>
2017-12-29 09:44:22 +0800

    1) init demo base on table view

---
+++ Demo(base on table view)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo(base on table view)
+//
+//  Created by Xuzixiang on 2017/12/29.
+//  Copyright © 2017年 frankxzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo(base on table view)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo(base on table view)
+//
+//  Created by Xuzixiang on 2017/12/29.
+//  Copyright © 2017年 frankxzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
